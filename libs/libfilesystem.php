b1b7f97eb42dae6a80f1e30da5f555df05eeb679
Jose V Beneyto <sepen@crux.nu>
2009-05-25 21:43:54 +0000

    libfilesystem.php: Initial import
    git-svn-id: file:///tmp/pmikro-svnrepo/trunk@29 f65f65b4-ad0d-11dd-b3f0-c11ec9ce7592

---
+++ libs/libfilesystem.php
@@ -0,0 +1,35 @@
+<?php
+
+class Filesystem {
+
+  var $debug = 0;
+
+  function Filesystem($debug = 0) {
+    if (!empty($debug)) $this->debug = $debug;
+    return true;
+  }
+
+  function ls($path) {
+    $files = array();
+    $dirs = array();
+    if ($directory = opendir($path)) {
+      while ($f = readdir($directory)) {
+        if ($this->debug) echo "f: $f<br />\n";
+        $fpath = $path."/".$f;
+        if ($f != "." && $f != "..") {
+          if (is_dir($fpath)) {
+            array_push($dirs, $f);
+          }
+          elseif (is_file($fpath)) {
+            array_push($files, $f);
+          }
+        }
+      }
+      closedir($directory);
+    }
+    return array_merge($dirs, $files);
+  }
+
+}
+
+?>
