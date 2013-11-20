6cd1f42e7a3546311bdecb7eca5547e61bb994bd
Jose V Beneyto <sepen@crux.nu>
2013-11-20 20:36:55 +0100

    Navpanel now uses global var 'base_uri' instead of 'url'

--- includes/navpanel.php
+++ includes/navpanel.php
@@ -3,27 +3,27 @@
 $navpanel = array(
   array(
     'CAPTION' => 'main',
-    'HREF'    => $url."/"
+    'HREF'    => $base_uri
   ),
   array(
     'CAPTION' => 'page1',
-    'HREF'    => $url."/page1"
+    'HREF'    => $base_uri."page1.php"
   ),
   array(
     'CAPTION' => 'page2',
-    'HREF'    => $url."/page2"
+    'HREF'    => $base_uri."page2.php"
   ),
   array(
     'CAPTION' => 'page3',
-    'HREF'    => $url."/page3"
+    'HREF'    => $base_uri."page3.php"
   ),
   array(
     'CAPTION' => 'page4',
-    'HREF'    => $url."/page4"
+    'HREF'    => $base_uri."page4.php"
   ),
   array(
     'CAPTION' => 'page5',
-    'HREF'    => $url."/page5"
+    'HREF'    => $base_uri."page5.php"
   )
 );
 
