0eeebdceb283dc899bb489de0de3c10b0c6b549b
Jose V Beneyto <sepen@crux.nu>
2015-10-03 01:51:47 +0200

    Added more examples of libtemplate features. New JSON output example

--- app/routes/page1.php
+++ app/routes/page1.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Class page1Controller
+ */
 Class page1Controller extends pmikro implements iPmikroController {
 
     public static function getRoot() {
@@ -13,6 +16,13 @@ Class page1Controller extends pmikro implements iPmikroController {
         self::$appOutput = '<html><body style="margin: 20px 0; text-align: center">'
             . '<h1>Test page</h1><hr><p>Request: GET /page1/foo</p>'
             . '<p>Source: routes/page1.php (getFoo)</p></body></html>';
+        self::out();
+    }
+
+    public static function getExamplejson() {
+        $template = new Template('example', 'json');
+        $template->setVars(['EXAMPLE_VALUE' => 'This is an example of substituted value']);
+        self::$appOutput = $template->getContents();
         self::out();
     }
 }
