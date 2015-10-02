0f4a82d3c879c9d40cca1a1b757cf988ab3ed4e3
Jose V Beneyto <sepen@crux.nu>
2015-10-03 01:32:56 +0200

    Reformatted code in navpanel's config

--- app/config/navpanel.php
+++ app/config/navpanel.php
@@ -1,10 +1,10 @@
 <?php
 
-$navpanel = array(array('CAPTION' => 'main',
-                        'HREF'    => '/',),
-                  array('CAPTION' => 'test',
-                        'HREF'    => 'test'),
-                  array('CAPTION' => 'page1',
-                        'HREF'    => 'page1'),
-                  array('CAPTION' => 'page2',
-                        'HREF'    => 'page2'));
+$navpanel = [['CAPTION' => 'main',
+              'HREF'    => '/',],
+             ['CAPTION' => 'test',
+              'HREF'    => 'test'],
+             ['CAPTION' => 'page1',
+              'HREF'    => 'page1'],
+             ['CAPTION' => 'page2',
+              'HREF'    => 'page2']];
