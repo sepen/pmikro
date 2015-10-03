6357764d95678ddfe4647d676d7ca3b7402b7f9e
Jose V Beneyto <sepen@crux.nu>
2015-10-03 02:28:44 +0200

    Refactored libtemplate and added some private functions

--- vendor/pmikro/libtemplate.php
+++ vendor/pmikro/libtemplate.php
@@ -78,26 +78,9 @@ class Template {
     private function render() {
         $patterns = [];
         $replaces = [];
-        foreach ($this->templateVars as $key => $value) {
+        foreach ($this->templateVars as $key=>$value) {
             if (is_array($value)) {
-                $regexp = '/{{loop: ' . $key . '}}\n'
-                    . '(.*\n)*'
-                    . '{{end_loop: ' . $key . '}}/';
-                preg_match($regexp, $this->templateOutput, $m);
-                if (isset($m[0])) {
-                    $loop = $m[0];
-                    $loop = preg_replace('/{{loop: ' . $key . '}}/', '', $loop);
-                    $loop = preg_replace('/{{end_loop: ' . $key . '}}/', '', $loop);
-                    $ltmp = "";
-                    foreach ($value as $va) {
-                        $loop_tmp = $loop;
-                        foreach ($va as $k => $v) {
-                            $loop_tmp = preg_replace('/{{' . $k . '}}/', $v, $loop_tmp);
-                        }
-                        $ltmp .= $loop_tmp;
-                    }
-                    $this->templateOutput = preg_replace($regexp, $ltmp, $this->templateOutput);
-                }
+                $this->renderLoop($key, $value);
             } else {
                 array_push($patterns, '/{{' . $key . '}}/');
                 array_push($replaces, $value);
@@ -106,4 +89,38 @@ class Template {
         $this->templateOutput = preg_replace($patterns, $replaces, $this->templateOutput);
     }
 
+    /**
+     * @param $key
+     * @param $value
+     */
+    private function renderLoop($key, $value) {
+        $regexp = '/{{loop: ' . $key . '}}\n'
+            . '(.*\n)*'
+            . '{{end_loop: ' . $key . '}}/';
+        preg_match($regexp, $this->templateOutput, $matches);
+        foreach($matches as $matchedLine) {
+            $loop = preg_replace('/{{loop: ' . $key . '}}/', '', $matchedLine);
+            $loop = preg_replace('/{{end_loop: ' . $key . '}}/', '', $loop);
+            $tmp = '';
+            foreach ($value as $va) {
+                $loop_tmp = $loop;
+                foreach ($va as $k=>$v) {
+                    $loop_tmp = $this->renderVar($loop_tmp, $k, $v);
+                }
+                $tmp .= $loop_tmp;
+            }
+            $this->templateOutput = preg_replace($regexp, $tmp, $this->templateOutput);
+        }
+    }
+
+    /**
+     * @param $contents
+     * @param $key
+     * @param $value
+     *
+     * @return mixed
+     */
+    private function renderVar($contents, $key, $value) {
+        return preg_replace('/{{' . $key . '}}/', $value, $contents);
+    }
 }
