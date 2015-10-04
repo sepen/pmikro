a13f225be70199198c325b8f0b9324a9fb482867
Jose V Beneyto <sepen@crux.nu>
2015-10-05 00:22:58 +0200

    Fixed require/require_once usage

--- pub/index.php
+++ pub/index.php
@@ -5,7 +5,7 @@ session_start();
 $appFile = __DIR__.'/../vendor/pmikro/pmikro.php';
 
 if (is_file($appFile)) {
-    require_once($appFile);
+    require($appFile);
 } else {
     die('ERROR: failed to load appFile: '.$appFile);
 }
